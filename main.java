7dec9e8f27da4c1e1c75b69825bd1db48686e31d
Dev Sakalley <sakalleydev27@gmail.com>
2025-06-16 12:17:07 +0530

    remove unwanted file

--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args){
-        // FIRST PROGRAMM
-        System.out.print("supp nigga!!\n");
-        System.out.print("how you doing!");
-    }
-}
